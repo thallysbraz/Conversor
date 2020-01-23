833ce41dbb57bd7c60baad431f3e7c0b02c32e04
thallysbraz <thallysbraz3@gmail.com>
2020-01-23 10:54:25 -0300

    Escritor de arquivos

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 
+fs.writeFile("./teste.json", "{idade: 20}", err => {
+  if (err) {
+    console.log("falhou na escrita");
+  }
+});
+
 fs.readFile("./teste.json", { encoding: "utf-8" }, (erro, dado) => {
   if (erro) {
     console.log("falhou");
