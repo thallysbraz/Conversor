1693e4e75d449bf239c4671604259db6a54fcc36
thallysbraz <thallysbraz3@gmail.com>
2020-02-04 10:34:37 -0300

    finalizando HTML

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const Reader = require("./Reader");
 const Processor = require("./Processor");
 const Table = require("./Table");
 const HtmlParser = require("./HtmlParser");
+const Writer = require("./Writer");
 
 var leitor = new Reader();
+var escritor = new Writer();
 
 async function main() {
   var dados = await leitor.Read("./Horas.csv"); //lendo arquivo
@@ -12,10 +14,12 @@ async function main() {
 
   var table = new Table(dadosProcessados);
 
-  console.log(table.rows);
-
   var html = await HtmlParser.Parse(table);
-  console.log(html);
+
+  const name = `${Date.now()}.html`;
+  console.log(name);
+
+  escritor.Write(name, html);
 }
 
 main();
