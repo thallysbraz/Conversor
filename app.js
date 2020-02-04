f626c015108c93710f8af14595287a41b08d893d
thallysbraz <thallysbraz3@gmail.com>
2020-02-04 09:30:55 -0300

    processador de dados

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var Reader = require("./Reader");
+var Processor = require("./Processor");
 
 var leitor = new Reader();
 
 async function main() {
   var dados = await leitor.Read("./Horas.csv");
-  console.log(dados);
+
+  Processor.Process(dados);
 }
 
 main();
